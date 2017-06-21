b004cb14fc0462f88df34deba319d3b41ab035d4
easp13 <easp13@gmail.com>
2017-06-21 07:21:14 +0300

    add http client app and api

---
+++ PicoHttpClient/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PicoHttpClient
+{
+    public partial class Form1 : Form
+    {
+        PicoClientApi pico;
+        public Form1()
+        {
+            InitializeComponent();
+
+            pico = new PicoClientApi("localhost");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string r;
+            if ((r = pico.Open()) != "ok")
+            {
+                MessageBox.Show(r);
+            }
+
+            timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string r;
+            if ((r = pico.Close()) != "ok")
+            {
+                MessageBox.Show(r);
+            }
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            float [] result;
+            textBox1.Clear();
+            if (pico.ReadChannels(out result)== "ok")
+            {                
+                for (int i = 0 ; i < result.Length ;i++)
+                {
+                    textBox1.Text += result[i].ToString() + Environment.NewLine;
+                }
+            }
+        }
+    }
+}
---
+++ PicoHttpClient/PicoClientApi.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VectonRestApi;
+using VectonRestApi.HttpUtils;
+
+namespace PicoHttpClient
+{
+    public class PicoClientApi
+    {
+        object m_lock = new object();
+        
+        RestClient m_restClient = new RestClient(); 
+        string m_url;
+        float[] m_channles = new float[8]; 
+
+        public PicoClientApi(string ip, int port = 8022)
+        {
+            m_url = "http://" + ip + ":" + port;
+        }
+
+      
+        public string ReadChannels(out float [] result)
+        {
+
+            lock (m_lock)
+            {
+                result = null;
+                try
+                {
+                    m_restClient.EndPoint = m_url + "/ReadChannels";
+                    m_restClient.Method = HttpVerb.GET;
+                    m_restClient.PostData = null;
+                    var json = m_restClient.MakeRequest();
+                    dynamic d = JObject.Parse(json);
+                    string r = d.Result;
+                    m_channles = Array.ConvertAll(r.Split(','), float.Parse);
+                    result = m_channles;
+                    return "ok";
+                }
+                catch (Exception err)
+                {
+                    return err.Message;
+                }
+            }
+        }
+  
+
+        public string Close()
+        {
+
+            lock (m_lock)
+            {
+                try
+                {
+                    m_restClient.EndPoint = m_url + "/Close";
+                    m_restClient.Method = HttpVerb.GET;
+                    m_restClient.PostData = null;
+                    var json = m_restClient.MakeRequest();
+                    dynamic d = JObject.Parse(json);
+                    string r = d.Result;
+                    return r;
+                }
+                catch (Exception err)
+                {
+                    return err.Message;
+                }
+            }
+        }
+
+        public string Open()
+        {
+
+            lock (m_lock)
+            {
+                try
+                {
+                    m_restClient.EndPoint = m_url + "/Open";
+                    m_restClient.Method = HttpVerb.GET;
+                    m_restClient.PostData = null;
+                    var json = m_restClient.MakeRequest();
+                    dynamic d = JObject.Parse(json);
+                    string r = d.Result;
+                    return r;
+                }
+                catch (Exception err)
+                {
+                    return err.Message;
+                }
+            }
+        }
+
+
+    }
+}
